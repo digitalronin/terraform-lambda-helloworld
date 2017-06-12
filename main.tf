64d002bfa560fec274ffba7d575208749b38b0db
David Salgado <david@digitalronin.com>
2017-06-12 14:40:19 +0200

    Finish the API integration
    When using the AWS console, we get a bunch of stuff for free, but
    with terraform we need to add all of the following by hand;
    
    * method reponse with model mapping
    * integration response with response template
    * permission for API gateway to invoke our lambda function
    
    For the final piece, we need the ID of our AWS account. This can
    be set as a variable (like the access key), but since we can
    derive it via terraform using aws_caller_identity, we may as well
    do that.

--- main.tf
+++ main.tf
@@ -78,3 +78,36 @@ REQUEST_TEMPLATE
   passthrough_behavior = "WHEN_NO_TEMPLATES"
 }
 
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = "${aws_api_gateway_rest_api.HelloWorldAPI.id}"
+  resource_id = "${aws_api_gateway_resource.HelloWorldResource.id}"
+  http_method = "${aws_api_gateway_method.HelloWorldPostMethod.http_method}"
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "HelloWorldPostIntegrationResponse" {
+  rest_api_id = "${aws_api_gateway_rest_api.HelloWorldAPI.id}"
+  resource_id = "${aws_api_gateway_resource.HelloWorldResource.id}"
+  http_method = "${aws_api_gateway_method.HelloWorldPostMethod.http_method}"
+  status_code = "${aws_api_gateway_method_response.200.status_code}"
+  response_templates {
+    "application/json" = ""
+  }
+}
+
+# Lambda permissions
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.helloworld.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.HelloWorldAPI.id}/*/${aws_api_gateway_method.HelloWorldPostMethod.http_method}/helloworldresource"
+}
