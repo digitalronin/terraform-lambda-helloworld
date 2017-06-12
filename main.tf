4ce36f72fa3c52b2ab88270e0db6b9cdd081a907
David Salgado <david@digitalronin.com>
2017-06-12 13:47:29 +0200

    Add API Gateway POST method

--- main.tf
+++ main.tf
@@ -49,3 +49,13 @@ resource "aws_api_gateway_resource" "HelloWorldResource" {
   parent_id   = "${aws_api_gateway_rest_api.HelloWorldAPI.root_resource_id}"
   path_part   = "helloworldresource"
 }
+
+# API Gateway POST Method
+
+resource "aws_api_gateway_method" "HelloWorldPostMethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.HelloWorldAPI.id}"
+  resource_id   = "${aws_api_gateway_resource.HelloWorldResource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
