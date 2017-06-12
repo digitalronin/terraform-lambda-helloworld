c1641ca150836d3e39899890a59ea7fc76281681
David Salgado <david@digitalronin.com>
2017-06-12 14:09:02 +0200

    Add API Gateway Integration
    This isn't working yet, via a CLI test. It's complaining about
    lambda permissions

--- main.tf
+++ main.tf
@@ -59,3 +59,22 @@ resource "aws_api_gateway_method" "HelloWorldPostMethod" {
   authorization = "NONE"
 }
 
+# API Gateway Integration
+
+resource "aws_api_gateway_integration" "HelloWorldPostIntegration" {
+  rest_api_id = "${aws_api_gateway_rest_api.HelloWorldAPI.id}"
+  resource_id = "${aws_api_gateway_resource.HelloWorldResource.id}"
+  http_method = "${aws_api_gateway_method.HelloWorldPostMethod.http_method}"
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.helloworld.arn}/invocations"
+  request_templates = {
+    "application/json" = <<REQUEST_TEMPLATE
+{
+  "name": "$input.params('name')"
+}
+REQUEST_TEMPLATE
+  }
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+}
+
