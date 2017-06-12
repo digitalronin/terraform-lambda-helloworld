01a4dd5f1199739882d601cf7d93e8ccd4fe14f0
David Salgado <david@digitalronin.com>
2017-06-12 13:24:11 +0200

    Add lambda function to the terraform code
    Create an IAM role and the lambda function, and
    add enable `make deploy` to build and deploy them

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+deploy: helloworld.zip
+	terraform apply
+
 helloworld.zip: helloWorld.js
 	zip -r9 helloworld.zip helloWorld.js
--- main.tf
+++ main.tf
@@ -3,3 +3,34 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
+
+# IAM Role for Lambda function
+resource "aws_iam_role" "helloworld_role" {
+    name = "helloworld_role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# AWS Lambda function
+resource "aws_lambda_function" "helloworld" {
+    filename = "helloworld.zip"
+    function_name = "helloWorld"
+    role = "${aws_iam_role.helloworld_role.arn}"
+    handler = "helloWorld.handler"
+    runtime = "nodejs6.10"
+    timeout = 3
+    source_code_hash = "${base64sha256(file("helloworld.zip"))}"
+}
